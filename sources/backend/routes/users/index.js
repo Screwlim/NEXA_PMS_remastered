791da3ea97b1be7865e6585a8df07e5734be6a12
Simon Lim <srlim1121@gmail.com>
2020-08-17 16:06:39 +0900

    fix router function name in index.js

--- sources/backend/routes/users/index.js
+++ sources/backend/routes/users/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const main = require('./login.js');
-const user = require('./signup.js')
+const login = require('./login.js');
+const signup = require('./signup.js')
 
-router.use('/login', main);
-router.use('/signup', user);
+router.use('/login', login);
+router.use('/signup', signup);
 
 module.exports = router;
