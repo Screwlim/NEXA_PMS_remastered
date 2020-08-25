8f972bc13c08ebed34a11c9117e9afb54ae5c0fd
Simon Lim <srlim1121@gmail.com>
2020-08-25 23:28:57 +0900

    [update] sign up function

--- sources/backend/api/routes/user/login.js
+++ sources/backend/api/routes/user/login.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../../db/models/index');
+const USER = db.USERS;
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  console.log("login process");
+router.get('/', function(req, res) {
+  console.log("login page");
   res.render('./user/auth-login.html');
 });
+
+router.post('/', function(req, res) {
+  console.log("login processing")
+  USER.findOne({
+    where: {
+      EMAIL: req.body.email
+    }
+  }).then(USER => {
+    res.redirect('/dashboard.html')
+  })
+})
 
 module.exports = router;
--- sources/backend/api/routes/user/signup.js
+++ sources/backend/api/routes/user/signup.js
@@ -1,10 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const db = require('../../db/models/index');
+const USER = db.USERS;
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log("signup process");
+  console.log("signup page");
   res.render('./user/auth-register.html');
+});
+
+router.post('/', function(req, res){
+  console.log('sign up in progress....');
+  USER.create({
+		NAME: req.body.name,
+		EMAIL: req.body.email,
+		COMPANY: req.body.company,
+		PASSWORD: bcrypt.hashSync(req.body.password, 8)
+	}).then(user => {
+      res.redirect('/index.html');
+	}).catch(err => {
+		res.status(500).send("Fail! Error -> " + err);
+	})
 });
 
 module.exports = router;
