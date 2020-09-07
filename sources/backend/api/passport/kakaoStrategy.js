77d4713b80b21af46793c60ebc0d0e129dfbc517
Simon Lim <srlim1121@gmail.com>
2020-09-07 17:42:50 +0900

    fix kakao login bug

--- sources/backend/api/passport/kakaoStrategy.js
+++ sources/backend/api/passport/kakaoStrategy.js
@@ -9,14 +9,16 @@ module.exports = (passport) => {
     try {
       const exUser = await USERS.findOne({ where: { snsId: profile.id, provider: 'kakao' } });
       if (exUser) {
-        done(null, exUser);
+        console.log(exUser.email + ' logging in auto');
+        done(null, exUser); //바로 로그인
       } else {
+        console.log('new user create from kakao');
         const newUser = await USERS.create({
-          EMAIL: profile._json && profile._json.kaccount_email,
-          SNSId: profile.id,
+          EMAIL: profile._json && profile._json.kakao_account.email,
+          SNSID: profile.id,
           PROVIDER: 'kakao',
         });
-        done(null, newUser);
+        done(null, newUser, {isNew : true}); //회원가입 후 로그인
       }
     } catch (error) {
       console.error(error);
