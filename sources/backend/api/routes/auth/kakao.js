953bed9d3e47cf122534ce1692d6ca49d5ef75f9
Simon Lim <srlim1121@gmail.com>
2020-09-09 11:32:39 +0900

    fix 3rd party sign up error

--- sources/backend/api/routes/auth/kakao.js
+++ sources/backend/api/routes/auth/kakao.js
@@ -7,11 +7,12 @@ router.get('/callback', passport.authenticate('kakao', {
     failureRedirect: '/auth/login',
 }),(req,res)=>{
     console.log('kakao auth succeeded');
-    if(req.user.NAME === null){
+    if(req.user.NAME === null || req.user.NAME === undefined){
         console.log('the user is newbie')
         res.redirect('/auth/signup-sns');
     } else {
         console.log('the user is ex-user')
+        console.log('user : ' + req.user.NAME)
         res.redirect('/dashboard');
     }
 });
