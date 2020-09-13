ae323cc18428f00a8c657a0b515504e4eb79ceff
Simon Lim <srlim1121@gmail.com>
2020-09-13 23:04:35 +0900

    add route for naver & kakao

--- sources/backend/api/routes/auth/index.js
+++ sources/backend/api/routes/auth/index.js
@@ -7,6 +7,8 @@ const signup_sns = require('./signup-sns.js')
 const password = require('./password.js')
 const logout = require('./logout.js')
 const kakao = require('./kakao.js')
+const naver = require('./naver.js')
+const google = require('./google.js')
 
 router.use('/login', login);
 router.use('/signup', signup);
@@ -14,5 +16,7 @@ router.use('/signup-sns', signup_sns);
 router.use('/password', password);
 router.use('/logout', logout);
 router.use('/kakao', kakao);
+router.use('/naver', naver);
+router.use('/google', google);
 
 module.exports = router;
