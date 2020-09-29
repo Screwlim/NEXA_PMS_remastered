211db8b336a85c21369061d35a42778a87415baf
Simon Lim <srlim1121@gmail.com>
2020-09-29 23:46:19 +0900

    init post api

--- sources/backend/api/routes/projects/new.js
+++ sources/backend/api/routes/projects/new.js
@@ -3,8 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log("project create process");
+  console.log("project create page");
   res.render('projects/new');
+});
+
+router.post('/', function(req, res, next) {
+  console.log("project create process");
+
+
+
+  console.log(req.body);
+  res.render('projects/new', {user: req.user});
 });
 
 module.exports = router;
