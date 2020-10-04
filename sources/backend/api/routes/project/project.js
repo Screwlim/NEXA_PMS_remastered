793fe81b7ef82aedd1aa34a1cf0b8610a7f97ce8
Simon Lim <srlim1121@gmail.com>
2020-10-05 03:28:30 +0900

    Updata: add job info passing

--- sources/backend/api/routes/project/project.js
+++ sources/backend/api/routes/project/project.js
@@ -1,22 +1,29 @@
 var express = require('express');
 var router = express.Router();
-const { PROJECTS } = require('../../db/models');
+const { PROJECTS, JOBS, TASKS } = require('../../db/models');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   console.log("project " + req.query.pid+ " process");
-
-  const getProj = async() => {
-    var proj = await PROJECTS.findOne({
-      where : {ID : req.query.pid}
-    });
+  var jobs;
+  var proj
+  PROJECTS.findOne({
+    where : {ID: req.query.pid}
+  }).then(data => {
+      jobs = JOBS.findAll({
+      where: {PROJECT_ID: data.ID}
+    })
+    proj = data
+  })
+  .then(data2 => {
+    if (data2 === undefined){
+      data2 = []
+    }
     console.log(proj)
+    console.log(jobs)
+    res.render('project/project', {proj: proj, jobs: data2, user: req.user});
+  })    
 
-    res.render('project/project', {proj: proj, user: req.user});
-    return 0;
-  }
-
-  getProj();
 
 });
 
