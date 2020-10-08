fd27033a6b7da4acbd4e6f9da4532a06a8bc0cef
Simon Lim <srlim1121@gmail.com>
2020-10-08 18:13:17 +0900

    Add: add job detail page route

--- sources/backend/api/routes/project/job/index.js
+++ sources/backend/api/routes/project/job/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const create = require('./create.js');
+const job = require('./job.js');
 
 router.use('/create', create);
+router.use('/', job);
 
 module.exports = router;
