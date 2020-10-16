1ef97459ba2a741f1ee50915348410d36d1591d4
Simon Lim <srlim1121@gmail.com>
2020-10-16 12:43:13 +0900

    Add: add create notice logic

--- sources/backend/api/routes/project/notices.js
+++ sources/backend/api/routes/project/notices.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { NOT } = require('sequelize/types/lib/deferrable');
 var router = express.Router();
 const { NOTICES } = require('../../db/models');
 
@@ -6,30 +7,46 @@ const { NOTICES } = require('../../db/models');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("project notice list process");
-  res.render('project/notice-list',{
-    user: req.user,
-    pid: req.query.pid
-  });
+
+  NOTICES.findAll({
+    where: {PROJECT_ID: req.query.pid}
+  }).then(data => {
+    res.render('project/notice-list',{
+      user: req.user,
+      pid: req.query.pid,
+      notices: data
+    });
+  }).catch(err => {
+    console.error(err);
+  })
+  
 });
 
 router.get('/detail', function(req, res) {
     console.log("project notice detail process");
 
-    const getNotice = async() => {
-      var results = await NOTICES.findAll({
-        where: {
-          CREATOR: req.user.ID
-        }
-    })};
-
-    const results = getNotice();
-    
-    console.log(results);
-    console.log(req.user);
-    res.render('project/notice-detail',{
-      user: req.user,
-      pid: req.query.pid
-    });
+    NOTICES.findOne({
+      where: {ID: req.query.nid}
+    }).then(data => {
+      res.render('project/notice-detail',{
+        user: req.user,
+        pid: req.query.pid,
+        notice: data
+      });
+    })
   });
+
+router.post('/', function(req,res) {
+  console.log('create notice')
+
+  NOTICES.create({
+    PROJECT_ID: req.query.pid,
+    TITLE: req.body.title,
+    CONTENT: req.body.content,
+    AUTHOR: req.user.ID,
+    AUTHOR_NAME: req.user.NAME,
+    FILEURL: 'url'
+  })
+})
 
 module.exports = router;
