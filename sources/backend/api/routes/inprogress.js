383e1830e3e68b5b16a5f7416515f6148d4053a6
Simon Lim <srlim1121@gmail.com>
2020-10-19 16:16:16 +0900

    Refactor: async code adjusted

--- sources/backend/api/routes/inprogress.js
+++ sources/backend/api/routes/inprogress.js
@@ -7,20 +7,15 @@ var router = express.Router();
 router.get('/', isLoggedIn,function(req, res) {
   console.log("in progress page");
 
-  const getProj = async() => {
-    var projlist = await PROJECTS.findAndCountAll({
-      include :[{
-        model: ATTENDANCES,
-        where: {USER_ID : req.user.ID}
-      }],
-      where : { [Op.or] : [{STATUS : 0}, {STATUS : -2}]}
-    });
-
+  PROJECTS.findAndCountAll({
+    include :[{
+      model: ATTENDANCES,
+      where: {USER_ID : req.user.ID}
+    }],
+    where : { [Op.or] : [{STATUS : 0}, {STATUS : -2}]}
+  }).then(projlist => {
     res.render('inprogress',{projs: projlist, user: req.user});
-    return projlist;
-  }
-
-  getProj();
+  })
 });
 
 module.exports = router;
