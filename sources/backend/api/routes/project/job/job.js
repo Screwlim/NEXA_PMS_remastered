8e8ad7868100d82accde9923fbddaf5629ab5102
Simon Lim <srlim1121@gmail.com>
2020-10-20 23:28:27 +0900

    Add: Add get Post-job query

--- sources/backend/api/routes/project/job/job.js
+++ sources/backend/api/routes/project/job/job.js
@@ -1,30 +1,35 @@
 var express = require('express');
 var router = express.Router();
-const {JOBS, TASKS} = require('../../../db/models');
+const {JOBS, TASKS, POST_JOB} = require('../../../db/models');
 /* GET users listing. */
 router.get('/', function(req, res) {
   console.log("job page");
 
   JOBS.findOne({
     where: {ID: req.query.jid}
-  }).then(Job => {
+  }).then(data => {
+    Job = data;
     TASKS.findAll({
       where: {JOB_ID: Job.ID}
-    }).then(Tasks => {
-      console.log(Job);
-      console.log(Tasks);
-
-      res.render('project/job/job',{
-        user: req.user,
-        pid: req.query.pid,
-        jid: req.query.jid,
-        job: Job,
-        tasks: Tasks,
-      });
+    }).then(data => {
+      Tasks = data
+      POST_JOB.findAll({
+        where: {JOB_ID: Job.ID}
+      }).then(Posts => {
+        console.log(Job);
+        console.log(Tasks);
+        console.log(posts);
+        res.render('project/job/job',{
+          user: req.user,
+          pid: req.query.pid,
+          jid: req.query.jid,
+          job: Job,
+          posts: Posts,
+          tasks: Tasks,
+        });
+      })
     })
-    
   })
-
 });
 
 module.exports = router;
