738795ef3c41475a5f5aa3d1ddae97355f5ebdde
Simon Lim <srlim1121@gmail.com>
2020-10-28 15:38:02 +0900

    Refactor: async programming done with then

--- sources/backend/api/routes/dashboard.js
+++ sources/backend/api/routes/dashboard.js
@@ -5,26 +5,23 @@ const { Op } = require('sequelize');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', isLoggedIn,function(req, res, next) {
+router.get('/', isLoggedIn,function(req, res) {
   var projData;
   
   console.log("dashboard process");
-  const getProj = async() => {
-    console.log("start searching");
-    projData = await PROJECTS.findAndCountAll({
+    PROJECTS.findAndCountAll({
       include :[{
         model: ATTENDANCES,
         where: {USER_ID : req.user.ID}
       }],
       where : { [Op.or] : [{STATUS : 0}, {STATUS : -2}]}
-    });
-    res.render('./dashboard',{
-      user : req.user,
-      proj : projData
-    });
-  }
-  //need 비동기 절차 조정
-  getProj();
+    }).then(data=>{
+      res.render('./dashboard',{
+        user : req.user,
+        proj : data
+      });
+    })
+    
 });
 
 module.exports = router;
