d2d4c163474ca9e34f08ceb188db95acc911099e
Simon Lim <srlim1121@gmail.com>
2020-11-17 11:47:32 +0900

    Update: add user profile update api

--- sources/backend/api/routes/user/profile.js
+++ sources/backend/api/routes/user/profile.js
@@ -1,10 +1,32 @@
 var express = require('express');
+const { USERS } = require('../../db/models');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   console.log("user profile");
-  res.render('user/profile');
+  res.render('user/profile', {user: req.user});
 });
+
+router.post('/', function(req, res) {
+  console.log("user profile update");
+
+  USERS.findOne({
+    where: {
+      ID: req.user.ID
+    }
+  }).then(data => {
+    data.NAME = req.body.name,
+    data.EMAIL = req.body.email,
+    data.COMPANY = req.body.company,
+    data.POSITION = req.body.position,
+    data.PHONENUMBER = req.body.phonenumber,
+    data.COMPANYNUMBER = req.body.companynumber
+    data.save();
+  }).then(data=>{
+    res.redirect('/user/profile');
+  })
+})
+
 
 module.exports = router;
