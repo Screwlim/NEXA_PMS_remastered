93952586b46c8a11f272a09a99d32e3ae01eb2a6
Simon Lim <srlim1121@gmail.com>
2020-11-17 16:05:30 +0900

    Update: Add invite page and API

--- sources/backend/api/routes/invites.js
+++ sources/backend/api/routes/invites.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
-const {INVITES, USERS} = require('../db/models');
+const {INVITES, USERS, PROJECTS, ATTENDANCES} = require('../db/models');
+const { Op, DataTypes } = require('sequelize');
 
 router.get('/', function(req, res) {
   console.log("invites list page");
 
   INVITES.findAll({
     include : [{
-      model :USERS,
-      attributes: ['NAME', 'COMPANY']
+      model : USERS
+    }, {
+      model: PROJECTS
     }],
-    where: {RECV_USER_ID: req.user.ID}
+
+    where: {RECV_USER_ID: req.user.ID, STATUS: 0}
   }).then(data => {
+    console.log(data);
     res.render('invites', {user: req.user, invites: data});
   })
 });
@@ -19,6 +23,30 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res){
   console.log("invite handle");
 
+  INVITES.findOne({
+    where : {
+      [Op.and] : [
+        {PROJECT_ID : req.body.invited_project}, 
+        {SEND_USER_ID : req.body.inviter_id}, 
+        {RECV_USER_ID: req.user.ID}
+      ]
+    }
+  }).then(data=> {
+    data.STATUS = req.body.answer;
+    data.save();
+    console.log(data);
+    if(req.body.answer == 1){
+      ATTENDANCES.create({
+        PROJECT_ID:req.body.invited_project,
+        USER_ID: req.user.ID,
+        ISPM: 0
+      })
+    }
+  }).then(data=>{
+    res.redirect('invites');
+  })
+  
+  
 });
 
 module.exports = router;
