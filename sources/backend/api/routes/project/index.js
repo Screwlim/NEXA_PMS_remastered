35793690371684e2e0f56525f1071aa377d40737
Simon Lim <srlim1121@gmail.com>
2020-12-01 16:53:30 +0900

    Add: Add project setting RU api

--- sources/backend/api/routes/project/index.js
+++ sources/backend/api/routes/project/index.js
@@ -6,11 +6,13 @@ const notices = require('./notices.js')
 const jobs = require('./jobs.js')
 const members = require('./members.js')
 const job = require('./job')
+const settings = require('./settings')
 
 router.use('/notices', notices);
 router.use('/jobs', jobs);
 router.use('/job', job);
 router.use('/members', members);
 router.use('/', proj);
+router.use('/settings', settings);
 
 module.exports = router;
