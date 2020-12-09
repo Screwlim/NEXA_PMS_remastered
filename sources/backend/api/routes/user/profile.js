570562f71d81c03c2dab516b6b1feb702c3bf8f5
Simon Lim <srlim1121@gmail.com>
2020-12-09 14:50:39 +0900

    Add: add profile img upload api

--- sources/backend/api/routes/user/profile.js
+++ sources/backend/api/routes/user/profile.js
@@ -1,6 +1,22 @@
 var express = require('express');
 const { USERS } = require('../../db/models');
 var router = express.Router();
+var multer = require('multer');
+var path = require('path');
+
+const upload = multer({
+  storage: multer.diskStorage({
+    //위치 지정
+    destination: (req, file, done) => {
+      done(null, "../web_frontend/src/assets/img/prof");
+    }, 
+    filename: (req, file, done) => {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename('profile_image_user'+req.user.ID, ext)+ ext);
+      // cb(null, new Date().valueOf() + path.extname(file.originalname));
+    }
+  })
+});
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -8,7 +24,7 @@ router.get('/', function(req, res) {
   res.render('user/profile', {user: req.user});
 });
 
-router.post('/', function(req, res) {
+router.post('/', upload.single('profileImage'), function(req, res) {
   console.log("user profile update");
 
   USERS.findOne({
@@ -16,11 +32,12 @@ router.post('/', function(req, res) {
       ID: req.user.ID
     }
   }).then(data => {
-    data.NAME = req.body.name,
-    data.COMPANY = req.body.company,
-    data.POSITION = req.body.position,
-    data.PHONENUMBER = req.body.phonenumber,
-    data.COMPANYNUMBER = req.body.companynumber
+    data.NAME = req.body.name;
+    data.COMPANY = req.body.company;
+    data.POSITION = req.body.position;
+    data.PHONENUMBER = req.body.phonenumber;
+    data.COMPANYNUMBER = req.body.companynumber;
+    data.prof_img_url = '../../assets/img/prof/'+req.file.filename;
     data.save();
   }).then(data=>{
     res.redirect('/user/profile');
