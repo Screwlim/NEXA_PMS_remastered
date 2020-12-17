06fef122140f4cac682d22c4288d6369a8fc4b0f
Simon Lim <srlim1121@gmail.com>
2020-12-17 09:54:57 +0900

    Update: setting request authorized to pm only

--- sources/backend/api/routes/projects/settings.js
+++ sources/backend/api/routes/projects/settings.js
@@ -22,21 +22,27 @@ router.get('/', function(req, res) {
 router.post('/', function(req,res) {
   console.log("project setting changed");
 
-  PROJECTS.findOne({
-    where : {
-      ID: req.query.pid
-    }
-  }).then(data =>{
-    console.log(req.body.start_date)
-    console.log(req.body.end_date)
-    data.NAME = req.body.name,
-    data.DESCRIPTION = req.body.projectDesc,
-    data.START_DATE = req.body.start_date,
-    data.END_DATE = req.body.end_date
-    data.save();
-  }).then(data =>{
-    res.redirect('/project?pid='+req.pid);
-  })
+  if(req.isPM){
+    PROJECTS.findOne({
+      where : {
+        ID: req.pid
+      }
+    }).then(data =>{
+      console.log(req.body.start_date)
+      console.log(req.body.end_date)
+      data.NAME = req.body.name,
+      data.DESCRIPTION = req.body.projectDesc,
+      data.START_DATE = req.body.start_date,
+      data.END_DATE = req.body.end_date
+      data.save();
+    }).then(data =>{
+      res.redirect('/project?pid='+req.pid);
+    })
+  }else{
+    res.status(403).render('./err/error-403.pug');
+  }
+
+  
 })
 
 module.exports = router;
