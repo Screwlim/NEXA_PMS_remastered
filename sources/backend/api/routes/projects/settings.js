f6f99ba2052ef953d1f6936daf98347729ea31b4
Simon Lim <srlim1121@gmail.com>
2020-12-22 10:06:12 +0900

    Add: project status (complete/cancel) handling api finished

--- sources/backend/api/routes/projects/settings.js
+++ sources/backend/api/routes/projects/settings.js
@@ -2,6 +2,35 @@ var express = require('express');
 var router = express.Router();
 const {PROJECTS} = require('../../db/models');
 /* GET users listing. */
+
+router.get('/status/:status', function(req, res){
+  if(req.isPM){
+    if(req.params.status == 'complete'){
+      PROJECTS.findOne({
+        where : {
+          ID: req.pid
+        }
+      }).then(data=>{
+        data.STATUS = 1; // Completed status value = 1
+        data.save()
+      }).then(()=>{
+        res.redirect('/proj_list');
+      })
+    }else if(req.params.status == 'cancel'){
+      PROJECTS.findOne({
+        where : {
+          ID: req.pid
+        }
+      }).then(data=>{
+        data.STATUS = -2; // Canceled status value = -2
+        data.save()
+      }).then(()=>{
+        res.redirect('/proj_list');
+      })
+    }
+  }
+});
+
 router.get('/', function(req, res) {
   console.log("프로젝트 관리 page");
   console.log(req);
@@ -28,8 +57,6 @@ router.post('/', function(req,res) {
         ID: req.pid
       }
     }).then(data =>{
-      console.log(req.body.start_date)
-      console.log(req.body.end_date)
       data.NAME = req.body.name,
       data.DESCRIPTION = req.body.projectDesc,
       data.START_DATE = req.body.start_date,
@@ -42,7 +69,10 @@ router.post('/', function(req,res) {
     res.status(403).render('./err/error-403.pug');
   }
 
-  
+
+
+
+
 })
 
 module.exports = router;
